2ebfa2248123bde1fba8ae0efe44b9b343a42aae
Rishav222222 <160915777+Rishav222222@users.noreply.github.com>
2024-06-25 17:50:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,11 +11,6 @@ function firstChar(text) {
     return text[0];
 }
 
-// Testing the function with various inputs
-console.log(firstChar(' Rosa Parks ')); // Output: 'R'
-console.log(firstChar(' Hello World ')); // Output: 'H'
-console.log(firstChar(' ')); // Output: ''
-console.log(firstChar('')); // Output: ''
 
 // Prompt user for input and display the result
 const userInput = prompt("Enter text:");
